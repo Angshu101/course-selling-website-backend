6bdddc0f68d45c1c85694ff039016e566f7f0753
Angshu101 <36377710+Angshu101@users.noreply.github.com>
2023-08-02 19:29:02 +0530

    Backend

--- index.js
+++ index.js
@@ -1,108 +1,144 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const {
-  User,
-  Course,
-  authenticateAdmin,
-  authenticateUser,
-  createJwtToken,
-  createAdmin,
-  createUser,
-  verifyTokenAdmin,
-  verifyTokenUser,
-  updateCourse,
-  getAllCourses,
-  getACourse,
-  adminRole,
-  userRole
-} = require('./utils');
+const express =require('express');
+const jwt=require('jsonwebtoken');
+const mongoose=require('mongoose');
+const app =express();
 
-const app = express();
-app.use(express.json());
+app.use(express.json())
 
-console.log({ connectionString: process.env.MONGODB_CONNECTION_STRING });
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
+const AdminSecret='eRtXyuwQio';
+const StudentSecret='SecTyens';
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+const teacherSchema=new mongoose.Schema({
+    username:String,
+    password:String,
+})
+const studentSchema=new mongoose.Schema({
+    studentname:String,
+    password:String,
+    purchasedCourses:[{type:mongoose.Types.ObjectId,ref:'Course'}]
 
-// Admin signup
-app.post('/admin/signup', (req, res) => {
-  createAdmin(req, res);
+})
+const courseSchema=new mongoose.Schema({
+    title:String,
+    description:String,
+    price:Number,
+    imageLink:String,
+})
+const Teacher = mongoose.model('Teacher', teacherSchema);
+const Student=mongoose.model('Student',studentSchema);
+const Course=mongoose.model('Course',courseSchema);
+
+const teacherjwtauth=(req,res,next)=>{
+const authenticateheader=req.headers.authorization;
+if(!authenticateheader){
+res.sendStatus(401)
+}
+else{
+    const token=authenticateheader.split(' ')[1];
+    jwt.verify(token,AdminSecret,(err,user)=>{
+        if(err){
+            res.sendStatus(403)
+        }
+            req.user=user;
+            next();
+    });
+}
+};
+const studentjwtauth=(req,res,next)=>{
+const authheader=req.header.authorization
+if(authheader){
+    const token=autheheader.split(' ')[1];
+    jwt.verify(token,StudentSecret,(err,student)=>{
+        if(err){
+            res.sendStatus(403)
+        }
+        req.student=student;
+        next()
+
+    })
+}else{
+    res.sendStatus(401)
+}
+};
+mongoose.connect('mongodb+srv://aminarahman01:N1sh4M0ng0@cluster0.utbunmm.mongodb.net/course_selling_website');
+
+app.post('/teacher/signup',async(req,res)=>{
+    const username=req.body;
+    const password=req.body;
+    //alternative const {username,password}=req.body
+    try{
+    const teacher=await Teacher.findOne({username})
+    if(teacher){
+        res.status(403).json({message:'Teacher Already Exists'});
+    }
+    else{
+        const obj={
+            username:username,
+            password:password,
+        }
+        const newTeacher=new Teacher(obj);
+        await newTeacher.save();
+        const token = jwt.sign({
+           username,
+           role:'admin', 
+        },AdminSecret,
+        {expiresIn:'5h'});
+        res.json({message:'Teacher Account Created Succesfully',token})
+    }
+    }
+    catch(error){
+        res.status(500).json({message:'Error processing request'});
+    }
+
 });
-
-// Admin login
-app.post('/admin/login', authenticateAdmin, (req, res) => {
-  const token = createJwtToken(req.headers.username, adminRole)
-  return res.status(200).json({ message: 'Logged in successfully', token })
+app.post('/teacher/login',async(req,res)=>{
+    const {username,password}=req.body;
+    const teacher=await Teacher.findone({username,password});
+    if(teacher){
+        const token=jwt.sign({username,role:'admin'},AdminSecret,{expiresIn:'5h'});
+        res.json({message:'loged in succesfuly as teacher'},token);
+    }
+    else{
+        res.status(403).json({message:'Invalid username or password'});
+    }
 });
+app.post('/student/signup',async(req,res)=>{
+    const {username,password}=req.body;
+    try{
+        const student=await Student.findOne({username});
+        if(student){
+            res.status(403).json({message:'student username already exists'});
+            const obj={
+                username:username,
+                password:password,
 
-// Admin create course
-app.post('/admin/courses', verifyTokenAdmin, async (req, res) => {
-  const course = new Course(req.body);
-  await course.save();
-  return res.json({ message: 'Course created successfully', courseId: course.id })
+            }
+            const newStudent=new Student(obj);
+            await newStudent.save();
+            const token=jwt.sign({
+                username,
+                role:'student',
+            },StudentSecret,{encoding:'5h'});
+         res.json({message:"Student Account created Succesfully"},token)
+        }
+        else{
+            res.status(403).json({ message: 'Invalid username or password' });
+
+        }
+    }
+    catch(error){
+        res.status(403).json({message:'Invalid username or password'});
+    }
 });
-
-// Admin edit/update course
-app.put('/admin/courses/:courseId', verifyTokenAdmin, (req, res) => {
-  updateCourse(req, res);
+app.post('/student/login',async(req,res)=>{
+    const {username,password}=req.body;
+    const student=await Student.findone({username,password});
+    if(student){
+        const token=jwt.sign({username,role:'student'},StudentSecret,{expiresIn:'5h'});
+        res.json({message:'loged in succesfuly as student'},token);
+    }
+    else{
+        res.status(403).json({message:'Invalid username or password'});
+    }
 });
-
-// Admin view all courses
-app.get('/admin/courses', verifyTokenAdmin, (req, res) => {
-  getAllCourses(res);
-});
-
-// User signup
-app.post('/users/signup', (req, res) => {
-  createUser(req, res);
-});
-
-// User login
-app.post('/users/login', authenticateUser, (req, res) => {
-  const token = createJwtToken(req.headers.username, userRole)
-  return res.status(200).json({ message: 'Logged in successfully', token })
-});
-
-// User view all courses
-app.get('/users/courses', verifyTokenUser, (req, res) => {
-  getAllCourses(res);
-});
-
-// User purchase a course
-app.post('/users/courses/:courseId', verifyTokenUser, async (req, res) => {
-  const courseId = req.params.courseId;
-  const course = await getACourse(courseId);
-  if (!course) {
-    return res.status(404).json({ message: 'Course not found' })
-  }
-  console.log({ course });
-
-  const username = req.user.username;
-  const user = await User.findOne({ username })
-  if (!user) {
-    return res.status(404).json({ message: 'User not found' })
-  }
-  const index = user.purchasedCourses.findIndex(id => id === courseId)
-  if (index === -1) {
-    return res.json({ message: 'Course already purchased' })
-  }
-  user.purchasedCourses.push(courseId)
-  await user.save();
-  return res.json({ message: 'Course purchased successfully' })
-});
-
-// User view purchased courses
-app.get('/users/purchasedCourses', verifyTokenUser, async (req, res) => {
-  const username = req.user.username;
-  const user = await User.findOne({ username })
-  if (!user) {
-    return res.status(404).json({ message: 'User not found' })
-  }
-  return res.json({ purchasedCourses: user.purchasedCourses || [] })
-});
-
-// these two lines are important for Vercel deployment
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+app.listen(3000, () => console.log('Server running on port 3000'));
